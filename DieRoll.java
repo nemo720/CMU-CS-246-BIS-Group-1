eb3f61448b22110128cc98ffd95d6a963a83afc8
LeAn3819 <lemanhan2132003@gmail.com>
2023-05-12 20:42:10 +0700

    LeManhAn commit

--- DieRoll.java
+++ DieRoll.java
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.âdasd
 
  */
 
@@ -31,6 +31,7 @@ public class DieRoll {
 	rnd=new Random();
     }
     public DieRoll(int ndice,
+    		
 		   int nsides,
 		   int bonus) {
 	this.ndice=ndice;
