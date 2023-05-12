74bc51fc3f0af5e37afa6e96d68052eaf390b744
LeAn3819 <lemanhan2132003@gmail.com>
2023-05-12 20:38:53 +0700

    LeManhAn commit

--- DieRoll.java
+++ DieRoll.java
@@ -1,3 +1,4 @@
+package cxfhh;
 import java.util.*;
 /*
 JDice: Java Dice Rolling Program
@@ -22,10 +23,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 public class DieRoll {
-    private int ndice
+    private int ndice;
     private int nsides;
     private int bonus;
-//    private static Random rnd;
+    private static Random rnd;
     static{
 	rnd=new Random();
     }
@@ -38,7 +39,7 @@ public class DieRoll {
     }
     public RollResult makeRoll() {
 	RollResult r=new RollResult(bonus);
-	for(int i 0;i<ndice;i++) {
+	for(int i =0;i<ndice;i++) {
 	    int roll=rnd.nextInt(nsides);
 	    r.addResult(roll);
 	}
@@ -49,10 +50,10 @@ public class DieRoll {
 	if(bonus>0) {
 	    ans= ans+"+"+bonus;
 	}
-	else (bonus<0) {
+	else if (bonus<0) {
 	    ans=ans+bonus;
 	}
-	return ans
+	return ans;
     }
 
 }
